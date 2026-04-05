ae4141cf8e0d2403122b1a567358c863e9e8f74c
elder lira <elderfranca@hotmail.com>
2026-04-05 17:21:45 -0300

    create core

---
+++ apps/core/mixins.py
@@ -0,0 +1,12 @@
+class CompanyQuerysetMixin:
+
+    def get_company_queryset(self, model):
+
+        user = self.request.user
+
+        if user.is_superadmin:
+            return model.objects.all()
+
+        return model.objects.filter(
+            company__company_users__user=user
+        )
